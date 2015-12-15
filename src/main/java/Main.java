8faa16314e1d7347a91560a971d0903b9ad607ca
Jonathan de Souza <jo_souza92@yahoo.com.br>
2015-12-15 16:01:14 -0200

    teste

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -13,11 +19,23 @@ public class Main{
 	public static void main( String[ ] args ) {
 		String texto = "<START:Pessoa> Marcelo <END> foi trabalhar em <START:Cidade> Piratininga <END>";
 
+		//TODO teste txt
+		StringBuffer sb = new StringBuffer();
+		Path p = Paths.get( "data/corpus_AMAZONIA.txt" );
+		try (Stream< String > lines = Files.lines( p )) {
+			lines.forEach( s -> sb.append( s ) );
+		} catch ( IOException ex ) {
+			ex.printStackTrace();
+		}
+		texto = new String( sb.toString().getBytes(), Charset.forName( "UTF-8" ) );//*/
+
 		String path = "data/treiner.ser";
 		CRFTrainer crfTrainer = new CRFTrainer( texto, path );
 		crfTrainer.treinar();
 
 		texto = "Juliana trabalha em Ubatuba";
+		texto = "depois se encontram com a dissidência do grupo , os Bacamarteiros de Pinga Fogo";
+
 		CRFClassifier classifier = new CRFClassifier( path );
 
 		List< Span > spans = classifier.classify( texto );
